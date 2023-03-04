314bcfdff9cbec0f61ed34ee828801e65a6ec4b0
leenh bustmante <hackingleenh@gmail.com>
2023-03-04 00:05:30 -0500

    organizacion de middlewares

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,12 +1,9 @@
 <?php
 
-// use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-// use Slim\Psr7\Response;
+// use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
-
+// Controllers
 use App\Controllers\HomeController;
 use App\Controllers\Login\ForgotPasswordController;
 use App\Controllers\Login\LoginController;
@@ -16,37 +13,14 @@ use App\Controllers\LogoutController;
 use App\Controllers\User\AccountController;
 use App\Controllers\WebController;
 
-$mw = function (Request $request, RequestHandler $handler) {
-    $response = $handler->handle($request);
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    if (!isset($_SESSION['lnh'])) {
-        return $response
-            ->withHeader('Location', base_url() . 'login')
-            ->withStatus(302);
-    }
-    return $response;
-};
-
-
-$lg = function (Request $request, RequestHandler $handler) {
-    $response = $handler->handle($request);
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    if (isset($_SESSION['lnh'])) {
-        return $response
-            ->withHeader('Location', base_url() . 'me')
-            ->withStatus(302);
-    }
-    return $response;
-};
+// Middlewares
+use App\Middleware\LoginMiddleware;
+use App\Middleware\RegisterMiddleware;
 
 $app->get('/', WebController::class . ':index');
-$app->get('/login', LoginController::class . ':index')->add($lg);
+$app->get('/login', LoginController::class . ':index')->add(new LoginMiddleware());
 $app->post('/login', LoginController::class . ':login');
-$app->get('/register', RegisterController::class . ':index')->add($lg);
+$app->get('/register', RegisterController::class . ':index')->add(new LoginMiddleware());
 $app->post('/register', RegisterController::class . ':save');
 $app->get('/verify-email/{url}', verifyController::class . ':index');
 $app->post('/email/verification-notification', verifyController::class . ':notification');
@@ -66,4 +40,4 @@ $app->group('/me', function (RouteCollectorProxy $group) {
     $group->get("", AccountController::class . ':index');
     $group->get("/account", AccountController::class . ':account');
     $group->get("/my-books", AccountController::class . ':edit');
-})->add($mw);
+})->add(new RegisterMiddleware());
